f45b3390d400ecfcf837a4edf696eca46bb67bad
Rafael Mendes <rafaelbemvindo@hotmail.com>
2024-10-16 22:30:29 -0300

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,13 +3,15 @@ import '../index.css'
 
 const HeroSection: React.FC = () => {
   return (
-    <section className='bg-hero-pattern bg-cover bg-center h-96 flex items-center justify-center text-white'>
+    <section className='bg-hero-pattern bg-cover bg-center h-96 flex items-center justify-center text-red-500'>
       <div className='text-center'>
         <h1 className='text-5xl font-bold mb-4'>
           <span className='header-logo'>PHD</span>
           <span className='header-tagline'>esenrola</span>
         </h1>
-        <p className='text-xl mb-8'>Encontre os melhores serviços em um só lugar</p>
+        <p className='text-xl mb-8'>
+          Encontre os melhores serviços em um só lugar
+        </p>
         <a href='#services' className='btn btn-primary'>
           Veja nossos serviços
         </a>
